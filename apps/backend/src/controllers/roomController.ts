4f1a39e2f2043187561cb2a518514659401fae30
Crazeecoder01 <121546558+Crazeecoder01@users.noreply.github.com>
2025-07-29 00:16:57 +0530

    test

--- apps/backend/src/controllers/roomController.ts
+++ apps/backend/src/controllers/roomController.ts
@@ -98,7 +98,7 @@ export const joinRoom: RequestHandler = async(req, res)=>{
         
      
         res.status(200).json({
-            message: 'User joined room successfully',
+            message: 'User joined the room successfully',
             room: {
                 id: room.id,
                 code: room.code,
